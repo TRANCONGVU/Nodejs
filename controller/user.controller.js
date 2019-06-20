ddcce11bd35d587af0af483c79680e9c9275be76
TRANCONGVU <vndvip10>
2019-06-21 06:07:23 +0700

    create user validation vu

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -35,6 +35,23 @@ module.exports.vu = function (req, res) {
 
 module.exports.postCreate = function (req, res) {
 	req.body.id = shortid.generate();
+	// -----bao mat kiem tra nhap vao --------
+	var errors = [];
+
+	if(!req.body.name){
+		errors.push('vui long nhap ten');
+	}
+	if(!req.body.phone){
+		errors.push('vui long nhap phone');
+	}
+	if(errors.length){
+		res.render('users/create',{
+			errors : errors,
+			values : req.body
+		});
+		return;
+	}
+// ----------------------------------------
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 };
